210dacef6304f04fafcc7dfc8f6fe98691e8743c
pichada00 <pichada.kon@gmail.com>
2022-09-13 18:53:28 +0700

    aa

--- climbing/Assets/Dias Games/Third Person System/Scripts/Interfaces/IDraggable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public interface IDraggable : IHandIKTarget, ICharacterTargetPos
-{
-    void StartDrag();
-    void StopDrag();
-
-    bool Move(Vector3 velocity);
-}
--- climbing/Assets/Dias Games/Third Person System/Scripts/Interfaces/IHandIKTarget.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public interface IHandIKTarget
-{
-    Transform GetLeftHandTarget();
-    Transform GetRightHandTarget();
-}
