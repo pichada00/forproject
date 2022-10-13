0e81cddafc00ac69915f39e8acb0015569bd7069
0rphnoch <numchai.auts@gmail.com>
2022-10-13 17:46:57 +0700

    Fade

---
+++ Assets/Scripts/Fade/FadeScene.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeScene : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeToNextLevel()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
